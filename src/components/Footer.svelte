a5df890574cb44f6e1d18508228321a2311da34b
carlosarribas <arribas@aisll.com>
2022-03-21 19:42:53 +0100

    netlify

---
+++ src/components/Footer.svelte
@@ -0,0 +1,20 @@
+<script>
+    let actualYear = new Date().getFullYear();
+</script>
+
+<style>
+  .Footer {
+    font-size: 11px;
+    letter-spacing: 1px;
+    font-weight: normal;
+    color: #c7c7c7;
+  }
+</style>
+  
+  <div class="Footer">
+    <div class="Footer-container">
+      <small class="Footer-content">
+        © { actualYear } Pugstagram from platzi
+      </small>
+    </div>
+  </div>
